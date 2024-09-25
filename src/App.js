b674096d29bc454b58ba880d87689ef1db3b03d2
Ravi30T <gatlaravi85@gmail.com>
2024-09-25 11:37:49 +0530

    Development Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import {Route, Switch} from 'react-router-dom'
+import { useState, useEffect, useContext } from 'react'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Home from './components/Home'
+import Profile from './components/Profile'
+import context from './context/context'
+import ProtectedRoute from './components/ProtectedRoute'
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const {userData} = useContext(context)
+
+  useEffect(() => {
+    const data = localStorage.getItem(userData)
+
+    if(data === null){
+      localStorage.setItem('userData', JSON.stringify(userData))
+    }
+
+  }, [userData])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path='/login' component = {Login} />
+      <Route exact path='/register' component={SignUp} />
+      <ProtectedRoute exact path='/' component={Home} />
+      <ProtectedRoute exact path="/profile" component={Profile} />
+    </Switch>
   );
 }
 
---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,60 @@
+import {useState} from 'react'
+
+import './index.css'
+
+const TodoItem = props => {
+  const {eachTodoItem, onDelete, onCheck, onEditTodo, onSaveTodo} = props
+  const {id, title, todoCompleted} = eachTodoItem
+
+  console.log(eachTodoItem)
+
+  const [saveBtn, updateSaveBtn] = useState(false)
+  const [newTodoStatus, updateNewTodoStatus] = useState('')
+
+  const onDeleteItem = () => {
+    onDelete(id)
+  }
+
+  const onClickCheck = e => {
+    updateNewTodoStatus(e.target.value)
+    onCheck(id, e.target.value)
+
+  }
+
+  const onEditItem = () => {
+    updateSaveBtn(prevState => !prevState)
+    onEditTodo(id)
+  }
+
+  const onSaveItem = () => {
+    updateSaveBtn(prevState => !prevState)
+    onSaveTodo(id)
+  }
+
+  return (
+    <li className='each-task'>
+        <p className='task-name'>{title}</p>
+        <div className='task-btn-container'>
+            {
+                saveBtn ? <button type='button' className='edit-btn' onClick={onSaveItem}>Save</button>: <button type='button' className='edit-btn' onClick={onEditItem}>Edit</button>
+            }
+
+            <select value={todoCompleted} onChange={onClickCheck} className='task-status-btn'>
+                        <option value="Pending">Pending</option>
+                        <option value="In Progress">In Progress</option>
+                        <option value="Done">Done</option>
+                        <option value="Completed">Completed</option>
+            </select>
+            
+            <button type='button' className='del-btn' onClick={onDeleteItem}>Delete</button>
+
+            
+        </div>
+
+        <div>
+        </div>
+    </li>
+  )
+}
+export default TodoItem
+
